e4973b9f7c7c733415dafaa490d369c147788fc3
naoki hirata <naoki@magic3.org>
2022-10-27 12:41:30 +0900

    コメント修正。

--- utils/session.ts
+++ utils/session.ts
@@ -13,16 +13,16 @@ export async function getSession(event: CompatibilityEvent) {
 	const app = useNitroApp()
 	const config = useRuntimeConfig()
 
+	// クッキー取得
 	const cookie = useCookies(event)[config.sessionCookieName]
-
 	if (!cookie) return null
 
+	// セッションID取得
 	const unsignedSession = unsign(cookie, config.sessionCookieSecret)
-
 	if (!unsignedSession) return null
 
+	// セッション情報取得
 	const user = await app.session.get(config.sessionIdPrefix + unsignedSession)
-
 	if (user) {
 		// セッションクッキーの有効期限更新
 		setCookie(event, config.sessionCookieName, cookie, {
