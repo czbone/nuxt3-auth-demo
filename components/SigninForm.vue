4c17cf1a29524c91150fe9bf1984a9b8eb212440
naoki hirata <naoki@magic3.org>
2023-06-12 08:30:54 +0900

    daisyui v3に対応。

--- components/SigninForm.vue
+++ components/SigninForm.vue
@@ -11,10 +11,8 @@
         <Form v-slot="{ errors }" :validation-schema="schema" @submit="validate">
           <div class="modal-body relative p-4">
             <div v-if="errorMessage" class="alert alert-error shadow-lg mb-4 rounded-lg">
-              <div>
-                <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                <span>{{ errorMessage }}</span>
-              </div>
+              <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+              <span>{{ errorMessage }}</span>
             </div>
             <!-- Email input -->
             <div class="mb-6">
